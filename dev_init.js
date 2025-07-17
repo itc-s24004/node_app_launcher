ebdd38d1d33358e114ed134769b72338dca61882
s24004 <s24004@std.it-college.ac.jp>
2025-07-17 11:12:51 +0900

    初期化処理修正

--- dev_init.js
+++ dev_init.js
@@ -1,16 +1,20 @@
-const { npm_install } = require("./system/LocalNode/main");
+const { NodeManager } = require("./system/LocalNode/main");
 const path = require("path");
 
 //ModuleRepositoryの補完ファイル生成
 require("./system/ModuleRepository/init");
 
 
-const installModule = [
-    path.join(__dirname, "system/GitHubAPI")
-];
-installModule.forEach(dir => {
-    ( async () => {
-        const {code} = await npm_install(dir);
-        if (code) throw new Error(`パッケージのインストールに失敗: ${dir}`)
-    })();
-})
+(async () => {
+    const local_node = await NodeManager.get("22.17.1");
+
+    const installModule = [
+        path.join(__dirname, "system/GitHubAPI")
+    ];
+    installModule.forEach(dir => {
+        ( async () => {
+            const {code} = await local_node.npm_install(dir);
+            if (code) throw new Error(`パッケージのインストールに失敗: ${dir}`)
+        })();
+    })
+})()
