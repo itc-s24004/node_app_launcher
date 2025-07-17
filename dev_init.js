0311ba70a802b5449dea5ab7549ebcd0162cf1a5
s24004 <s24004@std.it-college.ac.jp>
2025-07-17 11:24:31 +0900

    初期化自動化

--- dev_init.js
+++ dev_init.js
@@ -1,5 +1,8 @@
 const { NodeManager } = require("./system/LocalNode/main");
+const { ModuleReposiory } = require("./system/ModuleRepository/main");
+
 const path = require("path");
+const fs = require("fs");
 
 //ModuleRepositoryの補完ファイル生成
 require("./system/ModuleRepository/init");
@@ -16,5 +19,26 @@ require("./system/ModuleRepository/init");
             const {code} = await local_node.npm_install(dir);
             if (code) throw new Error(`パッケージのインストールに失敗: ${dir}`)
         })();
-    })
-})()
+    });
+
+
+
+
+    const rep = new ModuleReposiory();
+    rep.register("NodeManager", NodeManager);
+
+    const APP_Root = path.resolve("NodeAPP");
+    fs.readdirSync(APP_Root).forEach(DName => {
+        if (DName.startsWith("___")) return;
+
+        const init = path.join(APP_Root, DName, "init.js");
+        try {
+            require(init).run(rep);
+        } catch (e) {
+            console.error(`初期化エラー: ${init}`);
+            // console.log(e)
+        }
+    });
+
+
+})();
